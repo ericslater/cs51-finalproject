35361768f62b429c044be382923cd8eb18f74b5d
ericslater <ericslater@college.harvard.edu>
2014-05-02 22:59:07 -0400

    csv parsing 2

--- ericCsv.ml
+++ ericCsv.ml
@@ -9,27 +9,52 @@ open Array
   
 let file = "basketballSimple.csv"
 
-let read_file filename =
-  let lines = ref [] in
-  let change = open_in filename in
-  try
-    while true; do
-      lines := input_line change :: !lines
-    done; []
-  with End_of_file ->
-    close_in_noerr change;
-    List.rev !lines;;
 
-let csv_parser strl = 
-  match strl with
-  | [] -> failwith "error"
-  | z::tl -> for i = 0 to ((String.length z)-1) do
-	       if phys_equal (int_of_char z.[i]) 13 then z.[i] <- (char_of_int 44) else ();
-	     done	     
+let csv_to_data (file : string) (b : bool) : string array = 
+
+  let read_file filename =
+    let lines = ref [] in
+    let change = open_in filename in
+    try
+      while true; do
+	lines := input_line change :: !lines
+      done; []
+    with End_of_file ->
+      close_in_noerr change;
+      List.rev !lines in
+
+  let csv_parser strl = 
+    match strl with
+    | [] -> failwith "csv_parser error"
+    | z::tl -> for i = 0 to ((String.length z)-1) do
+		 if phys_equal (int_of_char z.[i]) 13 then z.[i] <- (char_of_int 44) else ();
+	       done; z in
+
+  let onereg (a : string) : string list = Str.split (Str.regexp ",") a in
+
+  let rec datamake (slist : string list) (a : bool) = 
+    if a then 
+      match slist with 
+      | [] -> [||]
+      | _::_::_::_::tl -> datamake tl false
+    else 
+      match slist with
+      | [] -> [||]
+      | hd::tl -> Array.append [|hd|] (datamake tl false) in
+
+  datamake (onereg (csv_parser (read_file file))) b
+		 
 ;;
+
+let here = csv_to_data "basketballSimple.csv" true;;
+
+  open_in "basketballSimple.csv";;
 
 
 let a = read_file file;;
+
+
+
 csv_parser a;
 
 List.map ~f:(fun x -> print_string x) a;;
@@ -51,23 +76,5 @@ let rec datamake slist a =
     | hd::tl -> Array.append [|hd|] (datamake tl false)
 ;;
 
-let arrayreggy = datamake reggy true;;
-
-(*  
-let () =
-  (* Read file and display the first line *)
-  let ic = open_in file in
-  try 
-    let line = input_line ic in  (* read line from in_channel and discard \n *)
-    let me = line in
-    print_endline me;          (* write the result to stdout *)
-    (*let me = stdout*)
-    (*flush stdout;*)                (* write on the underlying device now *)
-   (* close_in ic *)                 (* close the input channel *) 
-  
-  with e ->                      (* some unexpected exception occurs *)
-    close_in_noerr ic;           (* emergency closing *)
-    raise e                      (* exit with error: files are closed but
-                                    channels are not flushed *)
-  
-  (* normal exit: all channels are flushed and closed *) *)
+let arrayreggy = datamake reggy true;; 
+ *)
